4a015f7c77832d87fcf7d9a5d4fb963d2288bb47
kisumutalent <homiemusa@gmail.com>
2018-05-03 23:10:29 +0300

    [Feature #157291045] user auth

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request, url_for, session, flash
 
 app = Flask(__name__)
 
@@ -10,6 +10,23 @@ def home():
 @app.route('/welcome')
 def welcome():
 	return render_template('welcome.html')
+
+@app.route('/dashboard')
+def dashboard():
+	return render_template('dashboard.html')
+
+
+
+# login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	error = None
+	if request.method == 'POST':
+		if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+			error = 'Invalid credentials. Please try agian.'
+		else:
+			return redirect(url_for('dashboard'))
+	return render_template('login.html', error=error)
 
 
 

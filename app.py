141e6ceff44cb760091e0505c513d7278f494ad3
kisumutalent <homiemusa@gmail.com>
2018-05-06 18:55:07 +0300

    adding data and modifing app.py

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
 from flask import Flask, render_template, redirect, request, url_for, session, flash
 
+from functools import wraps
+
+from data import Articles
+
 app = Flask(__name__)
+
+
+app.secret_key = "my precious"
+
+Articles = Articles()
+
+def login_required(f):
+	@wraps(f)
+	def wrap(*args, **kwargs):
+		if 'logged_in' in session:
+			return f(*args, **kwargs)
+		else:
+			flash('You need to login first.')
+			return redirect(url_for('login'))
+	return wrap
+ 
+
 
 
 @app.route('/')
@@ -8,12 +29,22 @@ def home():
 	return render_template('index.html')
 
 @app.route('/welcome')
+@login_required
 def welcome():
 	return render_template('welcome.html')
 
 @app.route('/dashboard')
+@login_required
 def dashboard():
 	return render_template('dashboard.html')
+
+@app.route('/register')
+def register():
+	return render_template('register.html')
+
+@app.route('/meals')
+def meals():
+	return render_template('meals.html', articles=Articles)
 
 
 
@@ -25,9 +56,18 @@ def login():
 		if request.form['username'] != 'admin' or request.form['password'] != 'admin':
 			error = 'Invalid credentials. Please try agian.'
 		else:
-			return redirect(url_for('dashboard'))
+			session['logged_in'] = True
+			flash('You were just logged_in!')
+			return redirect(url_for('welcome'))
 	return render_template('login.html', error=error)
 
+
+
+@app.route('/logout')
+@login_required
+def logout():
+	session.pop('logged_in', None)
+	return redirect(url_for('home'))
 
 
 if __name__ == '__main__':
